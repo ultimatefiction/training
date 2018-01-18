b2e71d187fab90eeba2f180a7f2bc957c13eba5e
ultimatefiction <denivan@mera.ru>
2018-01-18 16:04:00 +0300

    reflections

--- src/main/java/com/veritas/bullshit/training/reflections/ReflectionTest.java
+++ src/main/java/com/veritas/bullshit/training/reflections/ReflectionTest.java
@@ -1,16 +1,5 @@
 package com.veritas.bullshit.training.reflections;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.util.Scanner;
-
-/**
- * This program uses reflection to print all features of a class.
- * @version 1.1 2004-02-21
- * @author Cay Horstmann
- */
 public class ReflectionTest
 {
 
